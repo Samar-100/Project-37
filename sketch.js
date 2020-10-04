6202f25deccc5d74c95fdea466c23316d0b88a18
Samar-100 <67325192+Samar-100@users.noreply.github.com>
2020-10-04 18:24:17 +0530

    solved the ground dissapearing

--- sketch.js
+++ sketch.js
@@ -92,9 +92,7 @@ function draw() {
 
     trex.velocityY = trex.velocityY + 0.8;
 
-    if (ground.x < 0) {
-      ground.x = ground.width / 2;
-    }
+    ground.velocityX = 2;
 
     spawnClouds();
     spawnObstacles();
